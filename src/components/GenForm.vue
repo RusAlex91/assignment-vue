81dead6a1b5984079925f8b9d2c0faad2427735e
RusAlex91 <aocrusalex91@yandex.ru>
2022-02-17 16:56:59 +0300

    basic layout

---
+++ src/components/GenForm.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="">
+    <form action="">
+      <input type="text" class="records-number" />
+      <input type="text" class="onpage-number" />
+      <input type="text" class="rand-words" />
+      <button class="apply"></button>
+      <button class="reset"></button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
---
+++ src/components/Table.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="table-wrapper">
+    <div class="table"></div>
+    <div class="table-controls">
+      <button class="pag-button">&rarr</button>
+      <button class="pag-button">&larr</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
