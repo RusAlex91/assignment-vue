e1de41cfb7169c2b168f9aa6285684658c16c613
RusAlex91 <aocrusalex91@yandex.ru>
2022-02-17 20:57:35 +0300

    basic table data fill implemented

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,6 +1,19 @@
 <template>
   <div class="table-wrapper">
-    <div class="table"></div>
+    <div class="table">
+      <table>
+        <tr>
+          <th>id</th>
+          <th>name</th>
+          <th>rand</th>
+        </tr>
+        <tr v-for="entry in this.dataTable" :key="entry.id">
+          <td>{{ entry.id }}</td>
+          <td>{{ entry.title }}</td>
+          <td>{{ entry.rand }}</td>
+        </tr>
+      </table>
+    </div>
     <div class="table-controls">
       <button class="pag-button">&rarr</button>
       <button class="pag-button">&larr</button>
@@ -9,7 +22,24 @@
 </template>
 
 <script>
-export default {}
+export default {
+  props: {
+    tableData: {
+      type: Array,
+      default: () => []
+    }
+  },
+  data () {
+    return {
+      dataTable: null
+    }
+  },
+  watch: {
+    tableData (newValue, oldValue) {
+      this.dataTable = this.tableData
+    }
+  }
+}
 </script>
 
 <style></style>
