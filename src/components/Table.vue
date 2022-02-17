2e8f6352c02e7ad4d321dac2bf559de542694201
RusAlex91 <aocrusalex91@yandex.ru>
2022-02-17 23:38:12 +0300

    table buttons and simple pagination

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -7,7 +7,7 @@
           <th>name</th>
           <th>rand</th>
         </tr>
-        <tr v-for="entry in computedObj" :key="entry.id">
+        <tr v-for="entry in updatedTableData" :key="entry.id">
           <td>{{ entry.id }}</td>
           <td>{{ entry.title }}</td>
           <td>{{ entry.rand }}</td>
@@ -15,8 +15,11 @@
       </table>
     </div>
     <div class="table-controls">
-      <button class="pag-button">&rarr</button>
-      <button class="pag-button">&larr</button>
+      <button @click="prevTablePage" class="pag-button">-5</button>
+      <button @click="nextTablePage" class="pag-button">+5</button>
+      <div class="pagination" v-for="page in pageCount" :key="page">
+        <button @click="toPage(page)">{{ page }}</button>
+      </div>
     </div>
   </div>
 </template>
@@ -36,20 +39,53 @@ export default {
   data () {
     return {
       dataTable: null,
-      limit: null
+      limit: 0,
+      updatedTableData: null,
+      updatedLimit: null,
+      pageCount: null
     }
   },
   watch: {
     tableData (newValue, oldValue) {
       this.dataTable = this.tableData
+      this.pageCount = this.tableData.length / this.tableLimit
     },
     tableLimit () {
       this.limit = this.tableLimit
+      this.updatedLimit = this.tableLimit
+      this.limitedData()
     }
   },
-  computed: {
-    computedObj () {
-      return this.limit ? this.dataTable.slice(0, this.limit) : this.dataTable
+  methods: {
+    prevTablePage () {
+      const newLimit = this.updatedLimit - this.limit
+      const prevLimit = this.updatedLimit - this.limit - this.limit
+      // if (newLimit === 0) {
+      //   newLimit = this.limit
+      // }
+      this.updateLimit(newLimit, prevLimit)
+      this.updatedLimit = newLimit
+    },
+    nextTablePage () {
+      const newLimit = this.updatedLimit + this.limit
+      const prevLimit = this.updatedLimit
+      this.updateLimit(newLimit, prevLimit)
+      this.updatedLimit = newLimit
+    },
+    updateLimit (newLimit, prevLimit) {
+      const dataTable = this.dataTable
+      this.updatedTableData = dataTable.slice(prevLimit, newLimit)
+    },
+    limitedData () {
+      const dataTable = this.dataTable
+      this.updatedTableData = dataTable.slice(0, this.updatedLimit)
+    },
+    toPage (num) {
+      debugger
+      const dataTable = this.dataTable
+      const prevLimit = (num - 1) * this.limit
+      const newLimit = prevLimit + this.limit
+      this.updatedTableData = dataTable.slice(prevLimit, newLimit)
     }
   }
 }
