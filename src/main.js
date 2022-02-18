fec8da69fc1f12701cf60b4954c3f29be54f3ea7
RusAlex91 <aocrusalex91@yandex.ru>
2022-02-18 23:52:13 +0300

    install bootsrap vue

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
-createApp(App).mount('#app')
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.config.productionTip = false
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+
+new Vue({
+  render: h => h(App)
+}).$mount('#app')
